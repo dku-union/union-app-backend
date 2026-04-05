934ac38589da5dd9a52d7d1cb759e3d3aa58f31f
JunSeo99 <an001209@gmail.com>
2026-04-05 19:46:53 +0900

    fix: MiniAppService Cacheable import 누락 복원

--- src/main/java/com/union/union/domain/miniapp/service/MiniAppService.java
+++ src/main/java/com/union/union/domain/miniapp/service/MiniAppService.java
@@ -24,6 +24,7 @@ import com.union.union.global.infra.gcs.GcsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
